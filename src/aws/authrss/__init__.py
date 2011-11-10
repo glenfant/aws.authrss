d877113278827e52bd424c9224c2c7d68046eff5
Gilles Lenfant <gilles.lenfant@gmail.com>
2011-11-10 11:59:23 +0100

    Added version info

--- src/aws/authrss/__init__.py
+++ src/aws/authrss/__init__.py
@@ -9,8 +9,18 @@ aws_authrss_messagefactory = MessageFactory(config.PACKAGENAME)
 
 LOG = logging.getLogger(config.PACKAGENAME)
 
+__version__ = None
 
 def initialize(context):
-    """Initializer called when used as a Zope 2 product."""
-    # Nothing at the moment.
+    """Initializer called when used as a Zope 2 product.
+    """
+    # Setting the version
+    import os
+    from Products.CMFPlone.utils import versionTupleFromString
+    global __version__
+
+    version_txt_path = os.path.join(os.path.dirname(__file__), 'version.txt')
+    version_txt = open(version_txt_path).read().strip()
+    __version__ = versionTupleFromString(version_txt)
+    LOG.info("version %s installed", version_txt)
     return
